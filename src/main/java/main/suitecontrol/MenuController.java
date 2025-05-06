0e2f3312f21974edc07b496580024b4b530ace2e
DanteSolorzano <solorzanodante19@gmail.com>
2025-05-06 11:47:12 -0600

    Add two scenes in javafx and make a connection with a button

--- src/main/java/main/suitecontrol/MenuController.java
+++ src/main/java/main/suitecontrol/MenuController.java
@@ -9,16 +9,18 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloController {
+public class MenuController {
     private Stage stage;
     private Scene scene;
     private Parent root;
 
-    public void SwitchToMenu(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("menu-view.fxml"));
+
+    public void SwitchToInicio(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 }
+
